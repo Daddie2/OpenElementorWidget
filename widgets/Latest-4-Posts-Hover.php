79079b292d7a5ffa66aa7fcc735c7615a56df0c8
Daddie2 <Thescimmia@gmail.com>
2024-02-14 23:18:22 +0100

    Update Latest-4-Posts-Hover.php

--- widgets/Latest-4-Posts-Hover.php
+++ widgets/Latest-4-Posts-Hover.php
@@ -1,94 +1,91 @@
 <?php
+class Latest_4_Posts_Hover_Widget extends \Elementor\Widget_Base {
 
-class Latest_4_Posts_Hover_Widget extends Elementor\Widget_Base {
+public function __construct($data = [], $args = null) {
+    parent::__construct($data, $args);
+}
 
-    public function __construct() {
-        parent::__construct();
+public function get_name() {
+    return 'latest-4-posts-hover';
+}
 
-        $this->init_control();
-    }
+public function get_title() {
+    return esc_html__('Latest 4 Posts Hover', 'latest-4-posts-hover');
+}
 
-    public function get_name() {
-        return 'latest-4-posts-hover';
-    }
+public function get_icon() {
+    return 'eicon-post-list';
+}
 
-    public function get_title() {
-        return esc_html__('Latest 4 Posts Hover', 'latest-4-posts-hover');
-    }
+public function get_categories() {
+    return ['basic'];
+}
 
-    public function get_icon() {
-        return 'eicon-post-list';
-    }
+protected function _register_controls() {
+    $this->start_controls_section(
+        'section_content',
+        [
+            'label' => esc_html__('Content', 'latest-4-posts-hover'),
+        ]
+    );
 
-    public function get_categories() {
-        return ['basic'];
-    }
+    $this->add_control(
+        'posts_per_page',
+        [
+            'label' => esc_html__('Number of Posts', 'latest-4-posts-hover'),
+            'type' => \Elementor\Controls_Manager::NUMBER,
+            'default' => 4,
+        ]
+    );
 
-    protected function init_control() {
-        $this->start_controls_section(
-            'section_content',
-            [
-                'label' => esc_html__('Content', 'latest-4-posts-hover'),
-            ]
-        );
+    $this->end_controls_section();
+}
 
-        $this->add_control(
-            'posts_per_page',
-            [
-                'label' => esc_html__('Number of Posts', 'latest-4-posts-hover'),
-                'type' => \Elementor\Controls_Manager::NUMBER,
-                'default' => 4,
-            ]
-        );
+protected function render() {
 
-        $this->end_controls_section();
-    }
+    $settings = $this->get_settings_for_display();
 
-    public function render() {
+    $args = [
+        'posts_per_page' => $settings['posts_per_page'],
+    ];
 
-        $settings = $this->get_settings_for_display();
+    $posts = get_posts($args);
 
-        $args = [
-            'posts_per_page' => $settings['posts_per_page'],
-        ];
+    if ($posts) {
+        echo '<div class="latest-4-posts-hover">';
 
-        $posts = get_posts($args);
-    
-        if ($posts) {
-            echo '<div class="latest-4-posts-hover">';
+        foreach ($posts as $post) {
+            $post_title = get_the_title($post->ID);
+            $post_content = wp_trim_words($post->post_content, 15);
+            $post_date = date('d/m/Y', strtotime($post->post_date));
+            $post_link = get_permalink($post->ID);
+            $post_thumbnail = get_the_post_thumbnail_url($post->ID);
 
-            foreach ($posts as $post) {
-                $post_title = $post->post_title;
-                $post_content = wp_trim_words($post->post_content, 15);
-                $post_date = date('d/m/Y', strtotime($post->post_date));
-                $post_link = get_permalink($post->ID);
-                $post_thumbnail = get_the_post_thumbnail_url($post->ID);
-
-                echo '
-                    <div class="card">
-                        <a href="' . $post_link . '" class="card-link">
-                            <div class="card-image" style="background-image: url(' . $post_thumbnail . ');">
-                                <div class="info">
-                                    <h1>' . $post_title . '</h1>
-                                    <p class="post-date">' . $post_date . '</p>
-                                    <p class="description">' . $post_content . '</p>
-                                </div>
+            echo '
+                <div class="card">
+                    <a href="' . $post_link . '" class="card-link">
+                        <div class="card-image" style="background-image: url(' . $post_thumbnail . ');">
+                            <div class="info">
+                                <h1>' . $post_title . '</h1>
+                                <p class="post-date">' . $post_date . '</p>
+                                <p class="description">' . $post_content . '</p>
                             </div>
-                        </a>
-                    </div>';
-            }
+                        </div>
+                    </a>
+                </div>';
+        }
 
-            echo '</div>';
-        }
-        else {
-            // Gestisci il caso in cui $posts non è un array valido
-            echo '<div class="error-message">';
-            echo esc_html__('Impossibile recuperare i post. Si è verificato un errore.', 'latest-4-posts-hover');
-            echo '</div>';
-        }
+        echo '</div>';
     }
+    else {
+        // Gestisci il caso in cui $posts non è un array valido
+        echo '<div class="error-message">';
+        echo esc_html__('Impossibile recuperare i post. Si è verificato un errore.', 'latest-4-posts-hover');
+        echo '</div>';
+    }
+}
 
-    public function get_style_depends() {
-		return [ 'latest-4-post'];
-	}
+public function get_style_depends() {
+    return [ 'latest-4-post'];
+}
 }
