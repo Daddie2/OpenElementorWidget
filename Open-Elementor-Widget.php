97c75a043ceaacb1301149fcd351ac2ee4a615eb
Daddie2 <Thescimmia@gmail.com>
2024-03-05 21:46:55 +0100

    Update Open-Elementor-Widget.php

--- Open-Elementor-Widget.php
+++ Open-Elementor-Widget.php
@@ -1,36 +1,37 @@
 <?php
 /*
-Plugin Name:orWidget
+Plugin Name:OpenElementorWidget
 Description: Custom widget for Elementor
 Version: 1.0
 Author: Davide
 */
 
 // Register the custom widgets with Elementor
-add_filter('get_the_excerpt', function ($excerpt) {
+add_filter('the_excerpt', function ($excerpt) {
 
-	$excerpt_length = 40; // Change excerpt length 
+    $excerpt_length = 40; // Change excerpt length 
 
-	global $post;
+    global $post;
 
-	if (
-		is_archive() || is_search()
-	) {
-		$post = get_post();
-	}
+    if (
+        is_archive() || is_search()
+    ) {
+        $post = get_post();
+    }
 
-	if (
-		has_excerpt($post)
-	) {
-	} else {
-		$content = get_the_content();
-		$first_word = substr($content, 0, strpos($content, ' '));
-		$content = substr($content, strpos($content, ' ') + 2);
-		$excerpt = wp_trim_words($content, $excerpt_length);
-		$excerpt = $first_word . ' ' . $excerpt;
-	}
+    if (has_excerpt($post)) {
+		echo "porva";
+    }
+	 else {
+        $content = get_the_content();
+        $first_word = substr($content, 0, strpos($content, ' '));
+        $content = substr($content, strpos($content, ' ') + 2);
+		echo $first_word;
+        $excerpt = wp_trim_words($content, $excerpt_length);
+        $excerpt = $first_word . ' ' . $excerpt;
+    }
 
-	return $excerpt;
+    return $excerpt;
 }, 10, 2);
 add_action('elementor/widgets/widgets_registered', 'register_OpenElementorWidget_widgets');
 
